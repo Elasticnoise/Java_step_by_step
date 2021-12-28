ab66ed112cd4cce61c14911a7c2cf911fd3f81f7
Elasticnoise <vs.tretyakov01@gmail.com>
2021-12-28 16:27:14 +0300

    add simple filling station program

--- src/data_time.java
+++ src/data_time.java
@@ -1,6 +1,6 @@
 import java.time.LocalDateTime;
 
-public class main {
+public class data_time {
     public static void main(String[] args) {
         System.out.println("Дата и время:");
         System.out.println(LocalDateTime.now());
