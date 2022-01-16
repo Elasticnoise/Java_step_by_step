33a235a1c5fb730bcae4cfadfa54a7e8b66237b3
Elasticnoise <vs.tretyakov01@gmail.com>
2022-01-16 15:25:48 +0300

    add new lesson 06(part 2) - ENUM

--- 06-lesson/src/ArithmeticCalculator.java
+++ 06-lesson/src/ArithmeticCalculator.java
@@ -1,2 +1,21 @@
 public class ArithmeticCalculator {
+    public final int firstNum;
+    public final int secondNum;
+
+    public ArithmeticCalculator(int firstNum, int secondNum) {
+        this.firstNum = firstNum;
+        this.secondNum = secondNum;
+
+    }
+
+    public int calculate(Operation type) {
+        if (type == Operation.ADD)
+            return firstNum + secondNum;
+        else if (type == Operation.SUBTRACT)
+            return firstNum - secondNum;
+        else if (type == Operation.MULTIPLY)
+            return firstNum * secondNum;
+        else
+            return 0;
+    }
 }
